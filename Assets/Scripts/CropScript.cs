57e0c7b10d8d02ce147f9b90ffaf0ee50fb560b4
Ilham Jalu Prakosa <ilham.jalu00@gmail.com>
2021-12-16 15:21:20 +0700

    Initial commit

---
+++ Assets/Scripts/CropScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class CropScript : MonoBehaviour
+{
+    public TMP_InputField tinggi, lebar;
+    int cTinggi, cLebar;
+    public RawImage cropImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cTinggi = Int32.Parse(tinggi.text);
+        cLebar = Int32.Parse(lebar.text);
+        var a = cropImage.GetComponent<RectTransform>();
+        a.sizeDelta = new Vector2(cLebar, cTinggi);
+    }
+}
---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    public string deviceName;
+    WebCamTexture wct;
+
+    public RawImage test;
+
+    // For photo varibles
+    public Texture2D heightmap;
+    public Vector3 size = new Vector3(100, 10, 100);
+
+    // For saving to the _savepath
+    string _SavePath = Application.streamingAssetsPath + "/"; //Change the path here!
+    int _CaptureCounter = 0;
+
+    // Use this for initialization
+    void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        deviceName = devices[0].name;
+        wct = new WebCamTexture(deviceName, 400, 300, 12);
+        GetComponent<Renderer>().material.mainTexture = wct;
+        wct.Play();
+   }
+ 
+   public void TakeSnapshot()
+   {
+        Debug.Log("TEST");
+        Texture2D snap = new Texture2D(wct.width, wct.height);
+        Sprite tesSnap = Sprite.Create(snap, new Rect(0,0,wct.width, wct.height), new Vector2(0.5f, 0.5f));
+        snap.SetPixels(wct.GetPixels());
+        snap.Apply();
+
+        test.GetComponent<RawImage>().texture = snap;
+        heightmap = snap;
+
+        System.IO.File.WriteAllBytes(_SavePath + _CaptureCounter.ToString() + ".png", snap.EncodeToPNG());
+        ++_CaptureCounter;
+   }
+
+}
